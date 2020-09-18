e2939b64e41e8d481ec916843b60a11233e97689
iampeggypeg <peggyluvu@hotmail.com>
2020-09-18 10:59:54 +0800

    Revert baseURL for axios

--- src/axios-backend.js
+++ src/axios-backend.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'https://arkmindstreetlightbackend.azurewebsites.net/public/api',
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://arkmindstreetlightbackend.azurewebsites.net/public/api',
+    // baseURL: 'http://127.0.0.1:8000/api',
 })
 
 export default instance;
